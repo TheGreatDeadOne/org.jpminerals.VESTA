e76c5ef901bae75c8117f3dd751bfac46752643a
TheGreatDeadOne <gabrielelyas@protonmail.com>
2026-09-06 22:54:51 -0300

    Fix some things

--- vesta.sh
+++ vesta.sh
@@ -1,16 +1,19 @@
 #!/bin/sh
+
 export PATH="/app/jre/bin:$PATH"
 export JAVA_HOME="/app/jre"
-export LD_LIBRARY_PATH="/app/lib/VESTA:/app/lib/VESTA/PowderPlot:$LD_LIBRARY_PATH"
+export LD_LIBRARY_PATH="/app/lib/VESTA:/app/lib/VESTA/PowderPlot${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}"
 export GDK_BACKEND=x11
 
-# Cria o diretório de configuração do Flatpak e redireciona o .VESTA do usuário
 CONFIG_DIR="${XDG_CONFIG_HOME:-$HOME/.config}"
 mkdir -p "$CONFIG_DIR"
 
-# Garante que a pasta esperada pelo VESTA aponte para o local correto dentro da sandbox
-rm -rf "$HOME/.VESTA"
-ln -sf "$CONFIG_DIR" "$HOME/.VESTA"
+if [ -e "$HOME/.VESTA" ] && [ ! -L "$HOME/.VESTA" ]; then
+    mv "$HOME/.VESTA" "$CONFIG_DIR/.VESTA"
+fi
+
+rm -f "$HOME/.VESTA"
+ln -sf "$CONFIG_DIR/.VESTA" "$HOME/.VESTA"
 
 cd /app/lib/VESTA
 exec ./VESTA "$@"
